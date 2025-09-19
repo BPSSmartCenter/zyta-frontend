72c14748eb4262fc33e296ad6edb5e7fe7235ab2
Rakthai <rakthai.thanarom@gmail.com>
2025-09-19 10:40:45 +0700

    2025-09-19 10:40:45

--- src/data/Dashboard/notis.ts
+++ src/data/Dashboard/notis.ts
@@ -111,6 +111,17 @@ export const notis: Noti[] = [
 export const wellBeingNotis: Noti[] = [
   {
     type: "alert",
+    img: sleepingNoti,
+    titleKey: "notis.sleepingLong",
+    title: "ตรวจพบคนหลับนานกว่าปกติ",
+    site: "Site C",
+    coords: { lat: 13.7278956, lng: 100.52412349999997 },
+    date: "2025-09-19",
+    severity: "medium",
+    screenshot: sleepCamera,
+  },
+  {
+    type: "alert",
     img: fallingNoti,
     titleKey: "notis.fallDetected",
     title: "ตรวจพบคนล้ม",
