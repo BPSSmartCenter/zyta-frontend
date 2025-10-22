c5f7c065428eaa0efb6a82bcc9aeafe9e860eee5
Rakthai <rakthai.thanarom@gmail.com>
2025-10-22 14:55:30 +0700

    fix api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { isAxiosError } from "axios";
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
   withCredentials: true, // ให้ส่ง/รับ cookie (token) ไปกับ request
   headers: { "Content-Type": "application/json" },
 });
