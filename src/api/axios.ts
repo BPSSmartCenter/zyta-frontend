d63a2ba9d32e4f4be37a09bf0742bcb3c6fa8eee
North25228 <nuer25228@gmail.com>
2025-12-15 23:40:23 +0700

    edit api

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { isAxiosError } from "axios";
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}`,
+  baseURL: "http://203.159.95.162:3000/api",
   withCredentials: true, // ให้ส่ง/รับ cookie (token) ไปกับ request
   headers: { "Content-Type": "application/json" },
 });
--- src/api/iot.ts
+++ src/api/iot.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Relative URL to use Vite proxy
-const IOT_API_URL = "/api/devices";
+const IOT_API_URL = "http://203.159.95.162:3000/api/devices";
 
 export interface IoTDevice {
     id?: string | number;
